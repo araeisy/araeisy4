6cf3d8ee640b2b314e36af20a63a154c956324bf
araeisy <teleagentteam@gmail.com>
2016-05-30 14:44:13 +0430

    Update sudoers.lua

--- plugins/sudoers.lua
+++ plugins/sudoers.lua
@@ -3,10 +3,8 @@ do
 function run(msg, matches)
   return [[
 #Sudoers!
-  
-  1.@MehdiHS🌐 #Developer ( use #adddeveloper command to add #Mehdi_HS in your Groups & SuperGroups. )
-  
-  2.@YflQw🌐 #Manager ( use #addmanager command to add #Vandad_Yflqw in your Groups & SuperGroups. )
+
+The ONLY Bot SUDO id : @XHACKERX
   
 ]]
 end
